b09fb0d13e54d5bcaf8943ecf1ef282535c37f77
Juhyeon Lee <gogori6565@naver.com>
2022-05-18 18:47:35 +0900

    cours&title_Stream

--- Stream_Crawling.py
+++ Stream_Crawling.py
@@ -1,53 +1,8 @@
 ##활동스트림에서 코스이름, 타이틀 내용 -> 제공예정/오늘/최근항목 으로 구분해서 가져오기 수행##
 
-#현재 3.14.1 selenium 버전 사용중
-from selenium import webdriver
-#타임 객체 선언
-from time import sleep
-import requests
-from bs4 import BeautifulSoup as bs
+#●처음~soup = bs(response, 'html.parser')까지 코드 생략●#
 
-##전역변수 선언##
-i=1 #로그인 예외처리 변수
-
-#크롬 웹 드라이버 경로
-driver = webdriver.Chrome('C:\Chrome_Driver\chromedriver.exe')
-
-#로그인 실패 예외처리 - 실패시 다시 로그인 / 성공시 넘어감
-while(i):
-    try:
-        #크롬으로 블랙보드 로그인 화면 접속
-        url = "https://ecampus.chungbuk.ac.kr/"
-        driver.get(url)
-
-        #아이디와 비밀번호 입력(0.5초씩 기다리기) - 너무 빠르면 대형 사이트는 트래픽 공격으로 인식
-        print('user_id : ')
-        user_id = input()
-        sleep(0.5)
-        driver.find_element_by_name('uid').send_keys(user_id)
-
-        print('user_pw : ')
-        user_pw = input()
-        sleep(0.5)
-        driver.find_element_by_name('pswd').send_keys(user_pw)
-
-        #Xpath
-        driver.find_element_by_xpath('//*[@id="entry-login"]').click()
-
-        #사이트 이동
-        driver.get('https://ecampus.chungbuk.ac.kr/ultra/stream')
-
-        #while반복 멈추기
-        i=0
-    except:
-        pass
-
-#활동스트림 내용 담은 element 가져오기
-sleep(8)
-response=driver.find_element_by_xpath('//*[@id="body-content"]').get_attribute('innerHTML')
-
-soup = bs(response, 'html.parser')
-
+##cours & title 크롤링##
 ##제공예정##
 upc_cours=soup.select('.js-upcomingStreamEntries.activity-group.columns.main-column>ul.activity-feed>li>div>div>div>div>div>a')
 upc_title=soup.select('.js-upcomingStreamEntries.activity-group.columns.main-column>ul.activity-feed>li>div>div>div>div>div.name>ng-switch>a')
